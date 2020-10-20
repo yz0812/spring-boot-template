75625bec961a51cb4a571ce80e4488b82618f568
yz0812 <yz_0812@outlook.com>
2020-10-20 20:15:31 +0800

    first commit

---
+++ src/main/java/com/yczn/pgzx/PgzxClientApplication.java
@@ -0,0 +1,13 @@
+package com.yczn.pgzx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgzxClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgzxClientApplication.class, args);
+    }
+
+}
